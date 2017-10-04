f6309dd33cf831ab200eb86d59f78f5b027dc421
Robert Sundström <robert.sundstrom@outlook.com>
2017-10-04 10:53:53 +0200

    Applied JS Standard

--- src/app2.ts
+++ src/app2.ts
@@ -1,24 +1,24 @@
-import { defaultEvaluator, render } from "./templating";
+import { defaultEvaluator, render } from './templating'
 
-const template = document.getElementById("template");
-const target = document.getElementById("target");
+const template = document.getElementById('template')
+const target = document.getElementById('target')
 
 const model = {
-    description: "Whats up",
-    greet: (name: string) => `Hello, ${name}!`,
-    title: "Hey!",
-};
+  description: 'Whats up',
+  greet: (name: string) => `Hello, ${name}!`,
+  title: 'Hey!'
+}
 
-const ctxt = render(target!, template!, model);
+const ctxt = render(target!, template!, model)
 
-model.description = "Test";
+model.description = 'Test'
 
 setTimeout(() => {
-    const updateBinding = (elem: Element, expr: string, context: any) => {
-        elem.innerHTML = defaultEvaluator(expr, context);
-    };
-    for (const { elem, expr, context } of ctxt.placeholders) {
+  const updateBinding = (elem: Element, expr: string, context: any) => {
+    elem.innerHTML = defaultEvaluator(expr, context)
+  }
+  for (const { elem, expr, context } of ctxt.placeholders) {
         // Update binding
-        updateBinding(elem, expr, context.data);
-    }
-}, 2000);
+    updateBinding(elem, expr, context.data)
+  }
+}, 2000)
--- src/customBinder.ts
+++ src/customBinder.ts
@@ -1,23 +1,23 @@
-import {  bind, bindExpression } from "./templating";
+import { bind, bindExpression } from './templating'
 
-const cache: any = {};
+const cache: any = {}
 
-const customEvaluator = (expr: string, context: any)  => {
-    let f = null;
-    if (expr in cache) {
-        f = cache[expr];
-    } else {
-        f = new Function("context", `with(context) { return ${expr}; }`);
-        cache[expr] = f;
-    }
-    return f(context);
-};
+const customEvaluator = (expr: string, context: any) => {
+  let f = null
+  if (expr in cache) {
+    f = cache[expr]
+  } else {
+    f = new Function('context', `with(context) { return ${expr}; }`)
+    cache[expr] = f
+  }
+  return f(context)
+}
 
 const customBindingHandler = (elem: Element, expr: string, context: any) => {
-    bindExpression(customEvaluator, elem, expr, context);
+  bindExpression(customEvaluator, elem, expr, context)
     // Here shorthand for: elem.innerHTML = customEvaluator(expr, context);
 
-    console.log("Handling expression:", expr);
-};
+  console.log('Handling expression:', expr)
+}
 
-export const myBinder = (elem: Element, data: any) => bind(elem!, data, customBindingHandler);
+export const myBinder = (elem: Element, data: any) => bind(elem!, data, customBindingHandler)
